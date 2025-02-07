55f0a315fff38445f967509e592bcbc2a73be89c
ricardocc_21 <rcanoc21@gmail.com>
2025-02-07 08:22:18 +0000

    Home.blade modificado y creacion de EjercicioController.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <p>{{ __('You are logged in!') }}</p>
+
+                        <div class="d-flex justify-content-around mt-3">
+                            <a href="{{ route('rubrica.create') }}" class="btn btn-primary">Crear Rúbrica</a>
+                            <a href="{{ route('ejercicio.create') }}" class="btn btn-success">Crear Ejercicio</a>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
