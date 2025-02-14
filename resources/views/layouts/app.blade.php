83900711ed52b9c3f4ed935c2acb90ce212ce234
ricardocc_21 <rcanoc21@gmail.com>
2025-02-14 08:09:55 +0000

    Merge rubricas_user into develop

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,9 +16,13 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+    <!-- Agregar tu archivo CSS aquí -->
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}"> <!-- Esta línea -->
+
     <!-- Scripts -->
     {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 </head>
+
 <body>
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
