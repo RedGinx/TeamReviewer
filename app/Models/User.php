387aa1580bc2fc4147f0aa17f595cadfaca797c9
ricardocc_21 <rcanoc21@gmail.com>
2025-02-14 09:10:56 +0000

    Avances con los roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\database\database\factories\UserFactory> */
-    use HasFactory, Notifiable;
+    /** @use HasFactory<\database\database\database\factories\UserFactory> */
+    use HasFactory;
+    use Notifiable;
+    use HasRoles;
+
+
+
 
     /**
      * The attributes that are mass assignable.
