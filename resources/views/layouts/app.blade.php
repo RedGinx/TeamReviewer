e6e72e5c9fc4c67c059e14f2ac023666658e7e1a
vagrant <llozanop2003@gmail.com>
2025-02-20 10:37:30 +0000

    New feature/auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,10 +37,12 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav me-auto">
-                    <!-- Agregar enlace a las rúbricas -->
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('rubricas.index') }}">Rúbricas</a>
-                    </li>
+                    @if(auth()->user()->hasRole('profesor'))
+                        <!-- Agregar enlace a las rúbricas -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('rubricas.index') }}">Rúbricas</a>
+                        </li>
+                    @endif
                 </ul>
 
                 <!-- Right Side Of Navbar -->
